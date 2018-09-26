2bab3ba35cbd4c2c01e03f4e447029626e7f0bf7
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-26 09:24:11 -0400

    Final Program

--- Palindromic Numbers/Program.cs
+++ Palindromic Numbers/Program.cs
@@ -10,6 +10,28 @@ namespace Palindromic_Numbers
     {
         static void Main(string[] args)
         {
+            string tempVar = "";
+
+            string tempVar2 = "";
+
+            for (int i = 10; i < 1000000; i++)
+            {
+                tempVar2 = "";
+
+                tempVar = Convert.ToString(i);
+
+                char[] varChar = tempVar.ToCharArray();
+
+                for (int j = varChar.Length - 1; j >= 0; j--)
+                {
+                    tempVar2 += tempVar[j];
+                }
+                if (Convert.ToString(i) == tempVar2)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
